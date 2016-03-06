74bb903d4ea743fe86135a3201c90902ce711a3d
Martin Petluš <martinpetlus@gmail.com>
2016-03-06 12:13:35 +0100

    Refactor GeneticAlgorithm

--- src/ga/GeneticAlgorithm.js
+++ src/ga/GeneticAlgorithm.js
@@ -14,6 +14,30 @@ class GeneticAlgorithm {
     });
   }
 
+  _runStep(entries) {
+    const newPopulation = [];
+
+    if (this.opts.eliteCount != null) {
+      newPopulation.push(...this._elite(entries));
+    }
+
+    this.selection.setEntries(entries);
+
+    while (newPopulation.length < this.opts.populationSize) {
+      const parents = this.selection.selectParents();
+
+      parents[0] = this.factory.clone(parents[0]);
+      parents[1] = this.factory.clone(parents[1]);
+
+      this.crossover.crossover(parents);
+      this.mutator.mutate(parents);
+
+      newPopulation.push(...parents);
+    }
+
+    return newPopulation;
+  }
+
   constructor(opts, factory, selection, crossover, mutator) {
     this.opts = opts;
     this.factory = factory;
@@ -44,35 +68,11 @@ class GeneticAlgorithm {
 
       if (entry.fitness >= 0) {
         utils.logger.log(this.factory.stringOf(entry.chromosome));
-        return;
+        break;
       }
 
-      this.population = this.runStep(entries);
+      this.population = this._runStep(entries);
     }
-  }
-
-  runStep(entries) {
-    const newPopulation = [];
-
-    if (this.opts.eliteCount != null) {
-      newPopulation.push(...this._elite(entries));
-    }
-
-    this.selection.setEntries(entries);
-
-    while (newPopulation.length < this.opts.populationSize) {
-      const parents = this.selection.selectParents();
-
-      parents[0] = this.factory.clone(parents[0]);
-      parents[1] = this.factory.clone(parents[1]);
-
-      this.crossover.crossover(parents);
-      this.mutator.mutate(parents);
-
-      newPopulation.push(...parents);
-    }
-
-    return newPopulation;
   }
 
 }
