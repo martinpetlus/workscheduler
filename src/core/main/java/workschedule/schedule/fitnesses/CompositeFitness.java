592c1b6326c30d6d5438d7ee9a7501d570f22576
Martin Petluš <martinpetlus@gmail.com>
2016-04-10 19:51:08 +0200

    Rename to more appropriate field name

--- src/core/main/java/workschedule/schedule/fitnesses/CompositeFitness.java
+++ src/core/main/java/workschedule/schedule/fitnesses/CompositeFitness.java
@@ -6,17 +6,17 @@ import java.util.ArrayList;
 import java.util.List;
 
 public final class CompositeFitness implements Fitness {
-    private final List<Fitness> children;
+    private final List<Fitness> delegates;
 
-    public CompositeFitness(final List<Fitness> children) {
-        this.children = new ArrayList<Fitness>(children);
+    public CompositeFitness(final List<Fitness> delegates) {
+        this.delegates = new ArrayList<Fitness>(delegates);
     }
 
     @Override
     public int fitness(final Chromosome chr) {
         int result = 0;
 
-        for (Fitness fitness: children) {
+        for (Fitness fitness: delegates) {
             result += fitness.fitness(chr);
         }
 
