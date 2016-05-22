9046e46864d463a1fda61b9eb41a93a5a08c5560
Martin Petluš <martinpetlus@gmail.com>
2016-05-22 19:22:03 +0200

    Refactor Mutation.java

--- src/core/main/java/workschedule/genetics/mutation/Mutation.java
+++ src/core/main/java/workschedule/genetics/mutation/Mutation.java
@@ -5,5 +5,7 @@ import workschedule.genetics.Chromosome;
 import java.util.List;
 
 public interface Mutation {
+    public static final double PROBABILITY = 0.01;
+
     public void mutate(List<Chromosome> population);
 }
--- src/core/main/java/workschedule/genetics/mutation/StandardMutation.java
+++ src/core/main/java/workschedule/genetics/mutation/StandardMutation.java
@@ -5,15 +5,13 @@ import workschedule.genetics.Chromosome;
 import java.util.List;
 
 public final class StandardMutation implements Mutation {
-    public static final double PROBABILITY = 0.01;
-
     @Override
     public void mutate(final List<Chromosome> population) {
         for (int i = 0; i < population.size(); i += 1) {
             Chromosome chromosome = population.get(i);
 
             for (int j = 0; j < chromosome.getLength(); j += 1) {
-                if (Math.random() < PROBABILITY) {
+                if (Math.random() < Mutation.PROBABILITY) {
                     chromosome.setParam(i, !chromosome.getParam(i));
                 }
             }
