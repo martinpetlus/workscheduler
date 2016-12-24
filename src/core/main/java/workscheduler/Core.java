5d6b5e5e0270045331dd3af7872a39390769b5c7
Martin Petluš <martinpetlus@gmail.com>
2016-12-24 15:05:20 +0100

    Revert jar from core

--- src/core/main/java/workscheduler/Core.java
+++ src/core/main/java/workscheduler/Core.java
@@ -1,4 +1,40 @@
 package workscheduler;
 
+import com.google.gson.Gson;
+
+import workscheduler.genetics.ChromosomeFactory;
+import workscheduler.schedule.WorkScheduleFactory;
+import workscheduler.genetics.GeneticAlgorithm;
+import workscheduler.genetics.GeneticAlgorithmOptions;
+import workscheduler.genetics.scaling.PositiveFitnessScaling;
+import workscheduler.genetics.selection.RouletteWheelSelection;
+import workscheduler.genetics.crossover.TwoPointCrossover;
+import workscheduler.genetics.mutation.StandardMutation;
+
+import workscheduler.schedule.options.SerializableScheduleOptions;
+
 public final class Core {
+  public static void main(String[] args) {
+    Gson gson = new Gson();
+
+    SerializableScheduleOptions opts = gson.fromJson(args[0], SerializableScheduleOptions.class);
+    System.out.println(opts);
+
+    ChromosomeFactory factory = new WorkScheduleFactory(opts.toScheduleOptions());
+
+    GeneticAlgorithm ga = new GeneticAlgorithm(
+      GeneticAlgorithmOptions.builder()
+        .setPopulationSize(200)
+        .setEliteCount(8)
+        .build(),
+      factory,
+      new RouletteWheelSelection(
+        new PositiveFitnessScaling()
+      ),
+      new TwoPointCrossover(),
+      new StandardMutation()
+    );
+
+    ga.run();
+  }
 }
