b5132ba25868fa3d47994505bba41be8272bd786
Martin Petluš <martinpetlus@gmail.com>
2016-12-24 18:10:23 +0100

    Add handling of workSchedule req

--- src/server/controllers/workSchedule.js
+++ src/server/controllers/workSchedule.js
@@ -1,9 +1,25 @@
 import { Router } from 'express';
+import { exec } from 'child_process';
 
 const router = Router();
 
-router.get('/', (req, res) => {
-  res.status(200).json({ weeks: ['week 1', 'week 2'] });
+router.post('/', (req, res) => {
+  res.status(200).end();
+
+  const opts = JSON.stringify(req.body);
+
+  exec(
+    `java -jar workscheduler.jar ${opts}`,
+    { cwd: `${__dirname}/../../../build/libs` },
+    (error, stdout) => {
+      if (error) {
+        console.error(`exec error: ${error}`);
+        return;
+      }
+
+      console.log(stdout);
+    }
+  );
 });
 
 export default router;
