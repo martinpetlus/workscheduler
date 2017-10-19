3a1a76282fa7e6d4ef7492cea9e2b8b99b8e81b7
Martin Petluš <martinpetlus@gmail.com>
2017-10-19 11:06:29 +0200

    Uninstall unused dev deps

--- webpack.config.js
+++ webpack.config.js
@@ -33,14 +33,6 @@ module.exports = {
         test: /\.html$/,
         loader: 'file?name=[name].[ext]',
       },
-      {
-        test: /\.scss$/,
-        loaders: [
-          'style?sourceMap',
-          'css?modules&importLoaders=1&localIdentName=[path]___[name]__[local]___[hash:base64:5]',
-          'sass',
-        ],
-      },
     ],
   },
   plugins: [
